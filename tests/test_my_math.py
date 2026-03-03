a39e911820edb65358393456f06f451eaa87e06f
Hanna Kiyko <kiyko.hanna1984@gmail.com>
2026-03-03 14:00:18 +0100

    Add unit tests for mathematical operations in test_my_math.py

---
+++ tests/test_my_math.py
@@ -0,0 +1,51 @@
+"""tests.test_main.py."""
+
+from typing import Literal
+
+import pandas as pd
+import pytest
+
+from app.modules.my_math import add, print_data, square, sub
+
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (10, 2, 12),
+        (20, 2, 22),
+        (0, 2, 2),
+    ],
+)
+def test_add(a, b, expected):
+    """Test that add returns the correct sum."""
+    assert add(a, b) == expected
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (10, 2, 8),
+        (20, 2, 18),
+        (0, 2, -2),
+    ],
+)
+def test_sub(a, b, expected):
+    """Test that sub returns the correct difference."""
+    assert sub(10, 5) == 5
+
+@pytest.mark.parametrize(
+    "a, expected",
+    [
+        (5, 25),
+        (0, 0),
+        (-4, 16),
+    ],
+)
+def test_square(a, expected):
+    """Test that square returns the correct result."""
+    assert square(a) == expected
+
+
+@pytest.fixture
+def test_print_data():
+    """Test that print data returns the correct result."""
+    return pd.DataFrame({"a": [1, 2, 3]})
