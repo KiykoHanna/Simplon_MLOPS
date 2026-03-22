07d9aef3458eecd62a6fb5f3b1fd5519a9fc0938
Hanna Kiyko <kiyko.hanna1984@gmail.com>
2026-03-22 19:43:03 +0100

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,44 +1,34 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Adapted for CI/CD pipelines
 
 import os
 import sys
 
+# -- Path setup --------------------------------------------------------------
 sys.path.insert(0, os.path.abspath("../../app_api"))
 sys.path.insert(0, os.path.abspath("../../app_front"))
 
+# -- Project information -----------------------------------------------------
 project = "Simplon_MLOps"
 copyright = "2026, Hanna Kiyko"
 author = "Hanna Kiyko"
 release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "myst_parser",
-    "sphinx_autodoc_typehints",
-    "sphinx.ext.autosummary",
+    "sphinx.ext.autodoc",           # core autodoc
+    "sphinx.ext.napoleon",          # supports Google & NumPy docstrings
+    "sphinx_autodoc_typehints",     # type hints
+    "sphinx.ext.autosummary",       # generate summary tables
+    "sphinx.ext.viewcode",          # add links to source code
+    "myst_parser",                  # Markdown support
 ]
+
+# Generate autosummary files automatically
 autosummary_generate = True
 
-templates_path = ["_templates"]
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = "furo"
-html_static_path = []
-
+# Type hints in descriptions
+autodoc_typehints = "description"
 autodoc_default_options = {
     "members": True,
     "undoc-members": False,
@@ -46,3 +36,25 @@ autodoc_default_options = {
     "special-members": "__init__",
     "inherited-members": True,
 }
+
+# Templates and exclude patterns
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# Myst parser options
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "substitution",
+    "tasklist",
+]
+myst_heading_anchors = 3
+
